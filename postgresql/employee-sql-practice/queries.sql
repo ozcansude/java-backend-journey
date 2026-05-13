def28cb9abb7bbbcef1c5ae03ea03f3be6621872
sude özcan <ozcanssudee@gmail.com>
2026-05-13 22:33:17 +0300

    feat: add complete PostgreSQL employee analytics queries

--- postgresql/employee-sql-practice/queries.sql
+++ postgresql/employee-sql-practice/queries.sql
@@ -32,17 +32,17 @@ WHERE is_active;
 -- 07.
     SELECT *
     FROM employees
-    WHERE !is_active;
+    WHERE NOT is_active;
 
 -- 08.
     SELECT employees.first_name, employees.last_name, departments.name,departments.location
     FROM employees
     FULL JOIN departments
-    ON departments.id = employees.department_id
+    ON departments.id = employees.department_id;
 
--- 09. (⭐️tekrar bak)
-    SELECT e.first_name, e.last_name,
-           m.first_name AS manager_first_name
+-- 09.
+    SELECT  e.first_name || ' ' || e.last_name AS employee_name,
+            m.first_name || ' ' || m.last_name AS manager_name
     FROM employees e
     LEFT JOIN employees m
         ON e.manager_id = m.id;
@@ -120,12 +120,13 @@ WHERE is_active;
     GROUP BY p.name, p.id;
 
 -- 21.
-    SELECT p.name, count(e.id)
-    FROM employee_projects ep
-        LEFT JOIN employees e
-        ON  e.id = ep.employee_id
-        LEFT JOIN public.projects p on ep.project_id = p.id
-    GROUP BY p.name;
+    SELECT
+    p.name AS project_name,
+    COUNT(ep.employee_id) AS employee_count
+    FROM projects p
+        LEFT JOIN employee_projects ep
+    ON p.id = ep.project_id
+    GROUP BY p.id, p.name;
 
 -- 22.
     SELECT d.name,
@@ -182,5 +183,101 @@ WHERE is_active;
     SELECT e.first_name || ' ' || e.last_name AS employee_name
     FROM employees e
         LEFT JOIN employee_projects ep ON e.id = ep.employee_id
-    GROUP BY e.first_name || ' ' || e.last_name, ep.employee_id
     HAVING ep.employee_id IS NULL;
+
+-- 30.
+    SELECT p.name AS project_name, COUNT(ep.employee_id) AS employee_count
+    FROM projects p
+        LEFT JOIN public.employee_projects ep on p.id = ep.project_id
+    GROUP BY p.name
+    ORDER BY count(ep.employee_id) DESC
+    LIMIT 1;
+
+-- 31.
+    SELECT e.first_name || ' ' || e.last_name AS employee_name, SUM(ep.hours_per_week) AS hours_per_week
+    FROM employee_projects ep
+        LEFT JOIN public.employees e on ep.employee_id = e.id
+    GROUP BY e.first_name, e.last_name
+    ORDER BY SUM(ep.hours_per_week) DESC
+    LIMIT 5;
+
+-- 32.
+    SELECT DISTINCT
+        e.first_name || ' ' || e.last_name AS employee_name,
+        d.name AS department_name
+    FROM employees e
+        JOIN public.projects p on e.department_id = p.department_id
+        JOIN public.departments d on e.department_id = d.id
+    WHERE p.status = 'active';
+
+-- 33.
+    SELECT d.name AS department_name ,d.annual_budget AS department_annual_budget, COALESCE(SUM(e.salary), 0) AS total_employee_salary
+    FROM departments d
+        LEFT JOIN public.employees e on d.id = e.department_id
+    GROUP BY d.id, d.annual_budget, d.name;
+
+-- 34.
+    SELECT d.name AS department_name,
+           d.annual_budget,
+           COALESCE(SUM(e.salary), 0) AS total_salary
+    FROM departments d
+        LEFT JOIN public.employees e on d.id = e.department_id
+    GROUP BY d.name,d.id, d.annual_budget
+    HAVING d.annual_budget > COALESCE(SUM(e.salary), 0);
+
+-- 35.
+    SELECT
+        p.name AS project_name,
+        COALESCE(AVG(e.salary),0) AS avg_salary_of_active_prjects
+    FROM projects p
+        LEFT JOIN employees e
+        ON e.department_id = p.department_id
+    WHERE p.status = 'active'
+    GROUP BY p.name, p.id;
+
+-- 36. Çalışanları maaş aralığına göre sınıflandır.
+    SELECT
+        e.first_name|| ' ' || e.last_name AS employee_name,
+        e.salary,
+        CASE
+            WHEN 0 <= e.salary AND e.salary<50000 THEN '0-50000'
+            WHEN 50000 <= e.salary AND e.salary<100000 THEN '50000-100000'
+            WHEN 100000 <= e.salary AND e.salary<150000 THEN '100000-150000'
+            WHEN 150000 <= e.salary AND e.salary<200000 THEN '150000-200000'
+            ELSE '200000+'
+        END AS salary_range
+    FROM employees e;
+
+-- 37.
+    SELECT d.name AS department_name, count(e.id) AS employee_count
+    FROM departments d
+        LEFT JOIN employees e
+        ON e.department_id = d.id
+    GROUP BY d.id,d.name;
+
+-- 38. Maaşı kendi departman ortalamasından yüksek olan çalışanları listele.
+    SELECT employee_name,
+           salary,
+           department_name,
+           avg_salary_of_department
+    FROM(
+        SELECT
+            e.first_name || ' ' || e.last_name AS employee_name,
+            e.salary,
+            d.name AS department_name,
+            AVG(e.salary) OVER(PARTITION BY e.department_id) AS avg_salary_of_department
+        FROM employees e
+            LEFT JOIN public.departments d on e.department_id = d.id
+        )
+    WHERE salary > avg_salary_of_department;
+
+
+-- 39.
+    SELECT p.name AS project_name, p.start_date
+    FROM projects p
+    WHERE EXTRACT(YEAR FROM start_date) = 2024;
+
+-- 40. Manager'ı olmayan çalışanları yani üst yöneticileri listele.
+    SELECT e.first_name|| ' ' || e.last_name AS employee_name
+    FROM employees e
+    WHERE e.manager_id IS NULL;
